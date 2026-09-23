045b531af7e034299e5b3c26164d23f7afe3a1fc
Sebastiaan Mannem <sebas@mannem.nl>
2026-09-23 20:19:17 +0200

    Cleaning unnecessary files

--- docker/stolon/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPTDIR=$(dirname $0)
-for SVC in etcd stolon_sentinel stolon_keeper stolon_proxy stolon_init; do
-  "${SCRIPTDIR}/start_${SVC}.sh" &
-  sleep 1
-done
-wait
--- docker/stolon/scripts/start_stolon_sentinel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$(id -un)" != postgres ]; then
-  su - postgres $0
-  exit
-fi
-SCRIPTDIR=$(dirname $0)
-eval $($SCRIPTDIR/config_stolon.sh | sed -e 's/#.*//' -e '/[a-zA-Z0-9]/!d' -e 's/^/export /')
-
-stolon-sentinel
